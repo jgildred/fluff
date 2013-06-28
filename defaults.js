cfaa7a1daf2d979531b9180933b96cefd11e115a
John Gildred <john@gildred.com>
2013-06-28 00:14:25 -0700

    config change so I can push to heroku for fluff.io site

--- defaults.js
+++ defaults.js
@@ -2,14 +2,14 @@
 // DEFAULT CONFIGURATION
 
 exports.Config = {
-  status        : 'Working',  // set to 'Initialize' to initialize DB with seed data
+  status        : 'Initialize',  // set to 'Initialize' to initialize DB with seed data
   site_name     : 'Web Site',
   smtp_service  : 'Gmail',
   smtp_username : 'user@domain.com',
   smtp_password : 'password',
   email_from    : 'noreply@domain.com',
-  app_service   : 'Custom Install',   // to run on Heroku, change this to 'Heroku' before push
-  db_service    : 'Other MongoDB',    // change to 'MongoLab' if using Heroku MongoLab addon
+  app_service   : 'Heroku',   // to run on Heroku, change this to 'Heroku' before push
+  db_service    : 'MongoLab',    // change to 'MongoLab' if using Heroku MongoLab addon
   db_uri        : 'mongodb://localhost:27017/fluff', // set this when using 'Other MongoDB'
   site_url      : 'http://localhost',
   server_port   : '3000',             // ignored when using Heroku
