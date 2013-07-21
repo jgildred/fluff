a288fee2f1b0e5219a232f992277da7f0a4a0931
John Gildred <john@gildred.com>
2013-07-21 10:44:30 -0700

    minor comment in config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@
 // This data is required for the app to run.
 
 exports.Info = {
-  initialize  : false,       // set to true to initialize DB with data from seed.js on startup
+  initialize  : false,       // Set this to true to initialize DB with data from seed.js on startup.
   app_service : 'Heroku',    // 'Heroku', 'AppFog' or 'Custom' (blank = 'Custom')
   db : {
     service   : 'MongoLab',   // 'MongoLab' or 'MongoDB' (blank = 'MongoDB')
-    uri       : 'mongodb://localhost:27017/fluff' // uri required when 'Custom' + 'MongoDB'
+    uri       : 'mongodb://localhost:27017/fluff' // Used when app_service is 'Custom' and db.uri is 'MongoDB'. Fluff will fall back to this uri if the Heroku or AppFog environment var is missing.
   }
 }
