cb452580c2ef7977bcaa67d3872de67dfbfa1b4b
John Gildred <john@gildred.com>
2013-07-21 10:22:17 -0700

    fluff.io push

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
 
 exports.Info = {
   initialize  : false,       // set to true to initialize DB with data from seed.js on startup
-  app_service : 'Custom',    // 'Heroku', 'AppFog' or 'Custom' (blank = 'Custom')
+  app_service : 'Heroku',    // 'Heroku', 'AppFog' or 'Custom' (blank = 'Custom')
   db : {
-    service   : 'MongoDB',   // 'MongoLab' or 'MongoDB' (blank = 'MongoDB')
+    service   : 'MongoLab',   // 'MongoLab' or 'MongoDB' (blank = 'MongoDB')
     uri       : 'mongodb://localhost:27017/fluff' // uri required when 'Custom' + 'MongoDB'
   }
 }
