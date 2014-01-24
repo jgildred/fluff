17acc6c70620dcb31b68e1513d90dafd95f6c481
John Gildred <john@gildred.com>
2014-01-24 13:37:54 -0800

    better cors

--- app.js
+++ app.js
@@ -311,7 +311,7 @@ var allowCrossDomain = function(req, res, next) {
         list.push(app.get('config').cors.whitelist);
       }
       res.header('Access-Control-Allow-Credentials', true);
-      res.header('Access-Control-Allow-Origin', whitelist.join(','));
+      res.header('Access-Control-Allow-Origin', list.join(','));
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS'); 
       res.header('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-API-Key'); 
       
