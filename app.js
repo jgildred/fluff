26e45f77c1717864560a9ccb86be700a81a45595
John Gildred <john@gildred.com>
2014-02-18 22:37:17 -0800

    push

--- app.js
+++ app.js
@@ -308,7 +308,7 @@ var toModel = function (model) {
 }
 exports.toModel = toModel;
 
-// Force SSL
+// Force SSL if required
 var forceSsl = function (req, res, next) {
   if (app.get('config').ssl) { 
       if (req.headers['x-forwarded-proto'] != 'https') {
@@ -1113,7 +1113,7 @@ var applyConfig = function (req, res, callback) {
 
   // Run all the setup routines with the latest config
   app.enable('trust proxy'); // To support proxies
-  app.use(forceSsl);         // Uses app.config ssl
+  //app.use(forceSsl);         // Uses app.config ssl
   app.use(allowCrossDomain); // Uses app.config cors
   setupMailer();             // Uses app.config smtp
   app.use(requireApiKey);    // Uses app.config api_key
