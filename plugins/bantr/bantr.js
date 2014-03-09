71df61e3b6a8d4fe28afd93ad18e381adb6ecf3b
John Gildred <john@gildred.com>
2014-03-09 03:47:33 -0700

    push

--- plugins/bantr/bantr.js
+++ plugins/bantr/bantr.js
@@ -97,7 +97,7 @@ var nlResponse = function (req, res, utterance) {
   if (result != "") {
     wordPos.lookupVerb(result, function (synset) {
       if (synset.length > 0) {
-        match = synset[0].synonyms[0];
+        match = synset.last.synonyms[0];
         var text = "Did you say " + result + ", as in " + match +"?";
         var url  = Plug.iSpeechUrlPrefix + encodeURIComponent(text);
         utteranceResponse(res, text, url);
