357cbf0c16ba51941f675f2b2b04bf8f56480491
John Gildred <john@gildred.com>
2014-12-22 10:39:52 -0800

    fixes for plugins

--- plugins/keypair.io/test_data.js
+++
@@ -1,4 +0,0 @@
-test_identity = {
-  name     : "John Doe",
-  image_url      : "http://s3.amazonaws.com/expressplay/images/keypair.io/test_identity.png",
-  notes          : "Nothing else is known about this person.",};
